d7bbc9b22e29a5f579ef651b333d63984e5a6e7a
AyushKazi <ayushkazishrestha@gmail.com>
2023-10-22 11:30:22 +0545

    Navigating between pages with Link

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <h1>Home</h1>
+      <p>
+        Go to <Link to="/allproducts">all prodcuts page</Link>
+      </p>
+    </>
+  );
 };
 
 export default Home;
