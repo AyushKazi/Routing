ca86f2a07c63bdd749792976b75f869d85556aff
AyushKazi <ayushkazishrestha@gmail.com>
2023-10-22 13:19:03 +0545

    Main Layout and nested routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./routes/Home";
 import AllProducts from "./routes/Products";
+import RootLayout from "./routes/Root";
 
 const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
-  { path: "/allproducts", element: <AllProducts /> },
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/allproducts", element: <AllProducts /> },
+    ],
+  },
 ]);
 
 function App() {
